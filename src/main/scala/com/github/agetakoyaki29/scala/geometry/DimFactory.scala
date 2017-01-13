d4a65c3da69d3b30180def94e19590f363d85487
Your Name <you@example.com>
2017-01-14 07:58:49 +0900

    fix val to def (kari)

--- src/main/scala/com/github/agetakoyaki29/scala/geometry/DimFactory.scala
+++ src/main/scala/com/github/agetakoyaki29/scala/geometry/DimFactory.scala
@@ -12,18 +12,18 @@ trait DimFactory[T <: Dim] {
   def apply(seq: Seq[Double]): T
   def apply(elem: Double): T = this.apply(Indices map {_ => elem})
 
-  val Zero: T = this.apply(0d)
-  val Infinity: T = this.apply(Double.PositiveInfinity)
-  val NaN: T = this.apply(Double.NaN)
+  def Zero: T = this.apply(0d)
+  def Infinity: T = this.apply(Double.PositiveInfinity)
+  def NaN: T = this.apply(Double.NaN)
 
   def E(idx: Int): T = this.apply(Zero.updated(idx, 1d))
   def F(idx: Int): T = this.apply(this.apply(1d).updated(idx, 0d))
 
   // ---- for validation ----
 
-  val NotNaN:      T => T = dim => { require(! dim.isNaN,      "required not NaN " + dim.getClass.getSimpleName);      dim }
-  val NotInfinite: T => T = dim => { require(! dim.isInfinite, "required not Infinite " + dim.getClass.getSimpleName); dim }
-  val NotZero:     T => T = dim => { require(! dim.isZero,     "required not Zero " + dim.getClass.getSimpleName);     dim }
-  val Identity:    T => T = identity
+  def NotNaN:      T => T = dim => { require(! dim.isNaN,      "required not NaN " + dim.getClass.getSimpleName);      dim }
+  def NotInfinite: T => T = dim => { require(! dim.isInfinite, "required not Infinite " + dim.getClass.getSimpleName); dim }
+  def NotZero:     T => T = dim => { require(! dim.isZero,     "required not Zero " + dim.getClass.getSimpleName);     dim }
+  def Identity:    T => T = identity
 
 }
