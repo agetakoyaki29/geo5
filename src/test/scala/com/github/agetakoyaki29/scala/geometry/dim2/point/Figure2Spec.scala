3e940c29f4db1c4eab1a08c7398c9bba7d2b0f5f
Your Name <you@example.com>
2017-01-16 23:05:53 +0900

    add Figure2Test

---
+++ src/test/scala/com/github/agetakoyaki29/scala/geometry/dim2/point/Figure2Spec.scala
@@ -0,0 +1,18 @@
+package com.github.agetakoyaki29.scala.geometry.dim2.point
+
+import org.scalatest.WordSpec
+
+
+class Figure2Spec extends WordSpec {
+  val O = Point2.O
+
+  val point1 =  Point2 ( 414.55,  9843.2)
+  val dir1 =    Dir2   (-68.844, -3.5487)
+  val range1 =  Range2 (-683.1,   53.487)
+  val corner1 = Corner2( 867.879, 486.84)
+
+  val line1 =   Line2  (Point2( 32.3152, -564.656), Dir2   (97.687,   68.77))
+  val circle1 = Circle2(Point2(-9843.964, 5.6876),  Range2 (644.446, -78.6877))
+  val aabb1 =   AABB2  (Point2( 57.84,    687.97),  Corner2(498.88,   49684.68))
+
+}
---
+++ src/test/scala/com/github/agetakoyaki29/scala/geometry/dim2/point/Figure2Test.scala
@@ -0,0 +1,23 @@
+package com.github.agetakoyaki29.scala.geometry.dim2.point
+
+import com.github.agetakoyaki29.scala.geometry.Delta.RichDouble
+
+
+class Figure2Test extends Figure2Spec {
+
+  "it.aabb contain it" in (pending)
+  "it.aabb is intersect it" in (pending)
+  "it same it" in (pending)
+  "it contain it" in (pending)
+  // "it is not intersect it" in (pending)
+  // "it same a figure, then it not intersect the figure" in (pending)
+
+  "it nearest any pt norm === it distance any pt" in (pending)
+  "it through (it nearest any pt)" in (pending)
+  "it through a pt, then it contain the pt" in (pending)
+
+  "it through (it intersect any figure)" in (pending)
+  "it is not intersect a figure, then it intersect the figure have size 0" in (pending)
+  "it is intersect a figure, then it intersect the figure don't have size 0" in (pending)
+
+}
